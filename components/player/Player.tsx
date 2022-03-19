8287bfbf54a5e1eb22715a21833a48f20effaee6
rgba <abgier.avraha@gmail.com>
2022-03-19 21:06:16 +1100

    basic player ui

--- components/player/Player.tsx
+++ components/player/Player.tsx
@@ -10,6 +10,7 @@ import Animated, {
 } from "react-native-reanimated";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Paper, Typography } from "../ui-kit/Themed";
+import { LinearGradient } from "expo-linear-gradient";
 
 // TODO: get dominant color from image
 const DOMINANT_COLOR = "#446070";
@@ -34,11 +35,22 @@ export function Player() {
     },
     onActive: (event, ctx: GestureContext) => {
       const newValue = ctx.startY - event.translationY;
-      currentHeight.value = newValue < playerHeight ? playerHeight : newValue;
+      currentHeight.value =
+        newValue < playerHeight
+          ? playerHeight
+          : newValue > screenHeight
+          ? screenHeight
+          : newValue;
 
-      if (currentHeight.value - ctx.startY > dragToggleDistance) {
+      if (
+        currentHeight.value - ctx.startY > dragToggleDistance &&
+        currentHeight.value > playerHeight
+      ) {
         isOpen.value = true;
-      } else if (currentHeight.value - ctx.startY < dragToggleDistance) {
+      } else if (
+        currentHeight.value - ctx.startY < dragToggleDistance &&
+        currentHeight.value < screenHeight
+      ) {
         isOpen.value = false;
       }
     },
@@ -134,27 +146,25 @@ export function Player() {
 
 function FullPlayer() {
   return (
-    <View
+    <LinearGradient
+      start={{ x: 0, y: 0.4 }}
+      colors={["rgba(0,0,0,0)", "rgba(0,0,0,0.5)"]}
       style={{
-        padding: 12,
         position: "absolute",
         left: 0,
         right: 0,
         top: 0,
         bottom: 0,
+        padding: 24,
       }}
     >
-      <SafeAreaView style={{ flex: 1, margin: 24 }}>
-        <Typography>Menu</Typography>
+      <SafeAreaView style={{ flex: 0.5 }}>
+        <Typography>
+          <Ionicons size={32} name="chevron-down"></Ionicons>
+        </Typography>
       </SafeAreaView>
       <Paper
-        style={{
-          flex: 5,
-          margin: 24,
-          backgroundColor: undefined,
-          alignItems: "center",
-          display: "flex",
-        }}
+        style={{ flex: 5.5, flexDirection: "row", backgroundColor: undefined }}
       >
         <Image
           style={{
@@ -164,10 +174,84 @@ function FullPlayer() {
           source={require("../../assets/images/albums/ma-drive-slow-art.jpg")}
         />
       </Paper>
-      <View style={{ flex: 4, margin: 24 }}>
-        <Typography>Controls</Typography>
+      <View
+        style={{
+          flex: 4,
+        }}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            marginBottom: 24,
+          }}
+        >
+          <View>
+            <Typography style={{ fontSize: 20, fontWeight: "bold" }}>
+              Easy
+            </Typography>
+            <Typography style={{ fontSize: 20, opacity: 0.7 }}>
+              Mac Ayres
+            </Typography>
+          </View>
+          <Ionicons size={32} name="heart" color="#3ae05e" />
+        </View>
+        <View style={{ flexDirection: "row", marginBottom: 8 }}>
+          <View
+            style={{
+              position: "absolute",
+              borderRadius: 24,
+              height: 12,
+              aspectRatio: 1,
+              backgroundColor: "#fff",
+            }}
+          />
+          <View
+            style={{
+              marginTop: 3,
+              flex: 1,
+              height: 6,
+              borderRadius: 6,
+              backgroundColor: "rgba(255,255,255,0.2)",
+            }}
+          />
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            marginBottom: 24,
+          }}
+        >
+          <Typography style={{ opacity: 0.7 }}>0:01</Typography>
+          <Typography style={{ opacity: 0.7 }}>-5:12</Typography>
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Typography>
+            <Ionicons name="shuffle" size={40} />
+          </Typography>
+          <Typography>
+            <Ionicons name="play-skip-back" size={40} />
+          </Typography>
+          <Typography>
+            <Ionicons name="play-circle" size={60} />
+          </Typography>
+          <Typography>
+            <Ionicons name="play-skip-forward" size={40} />
+          </Typography>
+          <Typography>
+            <Ionicons name="infinite" size={40} />
+          </Typography>
+        </View>
       </View>
-    </View>
+    </LinearGradient>
   );
 }
 
